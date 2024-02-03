df7bf9d6964a78309671eec7214f6a21d10f8fb2
osparking <jbpark03@gmail.com>
2024-02-03 19:57:02 +0900

    클래스 및 멤버 상응 테이블 및 열 지정

---
+++ src/main/java/space/bum/junit/hiber/model/Country.java
@@ -0,0 +1,40 @@
+package space.bum.junit.hiber.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "COUNTRY")
+public class Country {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
+  private int id;
+  
+  @Column(name = "NAME")
+  private int name;
+  
+  @Column(name = "CODE_NAME")
+  private int code_name;
+
+  public Country(int name, int code_name) {
+    super();
+    this.name = name;
+    this.code_name = code_name;
+  }
+}
