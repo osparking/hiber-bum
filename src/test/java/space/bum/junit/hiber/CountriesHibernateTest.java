1e3169a5b7a6fc584d0c2d1bdf99200e440d1256
osparking <jbpark03@gmail.com>
2024-02-04 10:43:30 +0900

    국가 목록 채취 기능 시험 메소드 조성 후반

--- src/test/java/space/bum/junit/hiber/CountriesHibernateTest.java
+++ src/test/java/space/bum/junit/hiber/CountriesHibernateTest.java
@@ -3,23 +3,46 @@ package space.bum.junit.hiber;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import space.bum.junit.hiber.model.Country;
 
 class CountriesHibernateTest {
-  
+
   private List<Country> expectedCountryList = new ArrayList<>();
   private List<Country> expected국포함CountryList = new ArrayList<>();
   public static final String[][] COUNTRY_INIT_DATA = { { "호주", "AU" },
       { "대한민국", "KR" }, { "일본", "JP" }, { "중국", "CN" },
       { "미국", "US" }, { "영국", "UK" } };
-  
+
+  private EntityManagerFactory emf;
+  private EntityManager em;
+
   @BeforeEach
   void setUp() {
     initExpectedCountryLists();
+    emf = Persistence.createEntityManagerFactory("manning.hibernate");
+    em = emf.createEntityManager();
+    em.getTransaction().begin();
+
+    for (int i = 0; i < COUNTRY_INIT_DATA.length; i++) {
+      var c = new Country(COUNTRY_INIT_DATA[i][0], COUNTRY_INIT_DATA[i][1]);
+      em.persist(c);
+    }
+    em.getTransaction().commit();
   }
-  
+
+  @AfterEach
+  public void dropDown() {
+    em.close();
+    emf.close();
+  }
+
   private void initExpectedCountryLists() {
     for (int i = 0; i < COUNTRY_INIT_DATA.length; i++) {
       var c = new Country(COUNTRY_INIT_DATA[i][0], COUNTRY_INIT_DATA[i][1]);
